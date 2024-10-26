64d6b9edd811ae835039811bf7ffc2dbcabee5ad
GabiFerraz <ferrazmgabi@gmail.com>
2024-10-26 15:08:07 -0300

    adjustments initial project

--- src/main/java/com/megaByteful/application/domain/ServiceProvider.java
+++ src/main/java/com/megaByteful/application/domain/ServiceProvider.java
@@ -3,7 +3,7 @@ package com.megaByteful.domain;
 public class ServiceProvider {
 	private Long id;
 	private String name;
-	private String cpfCnpj;
+	private String document;
 	private String phone;
 	private String beatyServices;
 	private String adress;
